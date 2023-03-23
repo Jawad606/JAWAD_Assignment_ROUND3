a96f99264e69c163f9df3351f4eda161a2bd3c46
darksoul <jawadmirza606@gmail.com>
2023-03-23 22:43:54 +0500

    update taskbackend https

--- src/features/InfoSlice.js
+++ src/features/InfoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const baseUrl = "http://taskbackend.kaswa.ai";
+const baseUrl = "https://taskbackend.kaswa.ai";
 
 export const fetchInfo = createAsyncThunk(
   "Info/fetchInfo",
